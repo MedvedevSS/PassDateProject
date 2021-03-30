247eb36522f2c311d485e14df7f036a9459c778a
devGod <devgod@mail.ru>
2021-03-30 21:14:49 +0300

    commit

--- PassDateProject/ViewController.swift
+++ PassDateProject/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var loginTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var resultlabel: UILabel!
+     
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "detailSegue", sender: nil)
     }
-
-
+    
+    @IBAction func unwindSegueToMailScreen(segue: UIStoryboardSegue) {
+        guard segue.identifier == "unwindSegue" else { return }
+        guard let svc = segue.source as? SecondViewController else { return  }
+        self.resultlabel.text = svc.label.text
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let  dvc = segue.destination as? SecondViewController else { return }
+        dvc.login = loginTF.text
+    }
 }
 
